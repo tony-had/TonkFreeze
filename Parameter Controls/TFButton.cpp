b968c553f4096b56992b77af8d4bf90d91573657
Tony Hadjiivanov <tony.hadjiivanov@gmail.com>
2019-10-16 00:28:34 +0300

    Set images for the custom image button

--- Parameter Controls/TFButton.cpp
+++ Parameter Controls/TFButton.cpp
@@ -9,3 +9,19 @@
 */
 
 #include "TFButton.h"
+
+TFButton::TFButton()
+{
+	setImages(true, true, true, ImageCache::getFromMemory(BinaryData::freeze_off_png, BinaryData::freeze_off_pngSize),
+		1.f, Colour::fromFloatRGBA(0.f, 0.f, 0.f, 0.f),
+		ImageCache::getFromMemory(BinaryData::freeze_off_png, BinaryData::freeze_off_pngSize),
+		1.f, Colour::fromFloatRGBA(0.f, 0.f, 0.f, 0.f),
+		ImageCache::getFromMemory(BinaryData::freeze_on_png, BinaryData::freeze_on_pngSize),
+		1.f, Colour::fromFloatRGBA(0.f, 0.f, 0.f, 0.f));
+	setClickingTogglesState(true);
+}
+
+TFButton::~TFButton()
+{
+}
+
