b83aead0fb6bcf816a7f4b850db464de5c861465
Tony Hadjiivanov <tony.hadjiivanov@gmail.com>
2019-10-22 11:59:42 +0300

    Add look and feel class skeleton

---
+++ Helpers/TFLookAndFeel.h
@@ -0,0 +1,26 @@
+/*
+  ==============================================================================
+
+    TFLookAndFeel.h
+    Created: 22 Oct 2019 11:42:02am
+    Author:  tonyh
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class TFLookAndFeel : public LookAndFeel_V4
+{
+public:
+	TFLookAndFeel()
+	{
+	}
+
+	TFLookAndFeel()
+	{
+	};
+
+};
